ce13d7e5acf0afc20464d41bca47a21f47b2a590
Катя <katelyn@internet.ru>
2024-02-17 23:11:35 +0300

    реализация метода 1

--- Template4335/Template4335/Antipova_Ekaterina_4335.xaml.cs
+++ Template4335/Template4335/Antipova_Ekaterina_4335.xaml.cs
@@ -23,5 +23,10 @@ namespace Template4335
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
