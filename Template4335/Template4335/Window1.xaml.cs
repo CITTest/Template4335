80b17936e1ae54ee5718aacf0cad0fcdcbf0ec30
SljusarD <sljusarde@gmail.com>
2024-02-19 01:46:03 +0300

    Добавил функционал

--- Template4335/Template4335/Window1.xaml.cs
+++ Template4335/Template4335/Window1.xaml.cs
@@ -1,23 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Template4335
 {
-    /// <summary>
-    /// Логика взаимодействия для Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
         public Window1()
@@ -27,7 +17,68 @@ namespace Template4335
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // Указываем путь к файлу Excel
+            string filePath = "путь_к_вашему_файлу.xlsx";
 
+            // Создаем экземпляр DataLoader
+            DataLoader dataLoader = new DataLoader();
+
+            // Загружаем данные из Excel и сохраняем в базу данных
+            dataLoader.LoadDataFromExcel(filePath);
+
+            // Выводим сообщение об успешном импорте данных
+            MessageBox.Show("Данные успешно импортированы из файла Excel и сохранены в базу данных.");
+        }
+
+        public class OrderContext : DbContext
+        {
+            public DbSet<Order> Orders { get; set; }
+        }
+
+        public class Order
+        {
+            public int Id { get; set; }
+            public string OrderCode { get; set; }
+            public DateTime CreationDate { get; set; }
+            public string ClientCode { get; set; }
+            public string Services { get; set; }
+        }
+
+        public class DataLoader
+        {
+            public void LoadDataFromExcel(string filePath)
+            {
+                Excel.Application excelApp = new Excel.Application();
+                Excel.Workbook excelWorkbook = excelApp.Workbooks.Open(filePath);
+                Excel._Worksheet excelWorksheet = excelWorkbook.Sheets[1];
+                Excel.Range excelRange = excelWorksheet.UsedRange;
+
+                int rowCount = excelRange.Rows.Count;
+                int colCount = excelRange.Columns.Count;
+
+                List<Order> orders = new List<Order>();
+
+                for (int i = 2; i <= rowCount; i++)
+                {
+                    Order order = new Order
+                    {
+                        OrderCode = excelRange.Cells[i, 2].Value2.ToString(),
+                        CreationDate = DateTime.FromOADate((double)excelRange.Cells[i, 3].Value2),
+                        ClientCode = excelRange.Cells[i, 4].Value2.ToString(),
+                        Services = excelRange.Cells[i, 5].Value2.ToString()
+                    };
+                    orders.Add(order);
+                }
+
+                excelWorkbook.Close();
+                excelApp.Quit();
+
+                using (var context = new OrderContext())
+                {
+                    context.Orders.AddRange(orders);
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
