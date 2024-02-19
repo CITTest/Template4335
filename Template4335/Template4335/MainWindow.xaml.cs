01c6a64ab4b3dca62d1bf8807713de8f8f5519b8
DilyaraD <dautova565@gmail.com>
2024-02-19 12:15:42 +0300

    добавлена кнопка

--- Template4335/Template4335/MainWindow.xaml.cs
+++ Template4335/Template4335/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Template4335
 {
