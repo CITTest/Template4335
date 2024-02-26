2ea458824e0eab0fd058e6fc41f42d58fa2757ae
Jiux12 <rotf228@gmail.com>
2024-02-26 14:23:08 +0300

    Настройка БД и Excel

--- Приложение 3 лаба/WpfApp1/MainWindow.xaml.cs
+++ Приложение 3 лаба/WpfApp1/MainWindow.xaml.cs
@@ -12,12 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
