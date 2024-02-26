08750b2eea7128703569a2da10160d44f1f263bc
LINA <thealexbarrows@gmail.com>
2024-02-26 11:43:06 +0300

    Подключила библиотеку, добавила кнопки для импорта и экспорта

--- Template4335/Template4335/4335_BARYSHEVA.xaml.cs
+++ Template4335/Template4335/4335_BARYSHEVA.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Template4335
 {
