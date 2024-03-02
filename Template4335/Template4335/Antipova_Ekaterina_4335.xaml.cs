3ec71d6fb033243e26af235d159f47d490ae0e82
Катя <katelyn@internet.ru>
2024-03-02 22:03:27 +0300

    добавила библиотеку для работы с json

--- Template4335/Template4335/Antipova_Ekaterina_4335.xaml.cs
+++ Template4335/Template4335/Antipova_Ekaterina_4335.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Text.Json;
 
 namespace Template4335
 {
