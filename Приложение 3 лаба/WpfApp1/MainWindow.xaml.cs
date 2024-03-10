4d2e19ecf46799b78f8b8bbed4e68e95307d503a
Jiux12 <rotf228@gmail.com>
2024-03-10 17:39:07 +0300

    Добавление интерфейса

--- Приложение 3 лаба/WpfApp1/MainWindow.xaml.cs
+++ Приложение 3 лаба/WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WpfApp1
@@ -21,6 +22,21 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnImport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
